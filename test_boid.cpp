f2f0d40dc1deb5d62f36c765807129622f66306a
Andreas Wachowski <mail@andreas-wachowski.de>
2025-01-18 12:21:46 +0100

    test(boid): extract tests into separate file

---
+++ test_boid.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+#include "boid.h"
+
+TEST(Boid, Constructor) {
+  Boid b(100, 200);
+  EXPECT_EQ(b.max_x, 100);
+  EXPECT_EQ(b.max_y, 200);
+}
+
+TEST(Boid, Update) {
+  Boid b(100, 200);
+  float x{b.x};
+  float y{b.y};
+  float vx{b.vx};
+  float vy{b.vy};
+
+  b.update();
+
+  EXPECT_EQ(b.x, x + vx);
+  EXPECT_EQ(b.y, y + vy);
+}
