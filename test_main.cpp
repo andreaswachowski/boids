de248000945a4b72ad9c5b7d02ed614d8ce4f360
Andreas Wachowski <mail@andreas-wachowski.de>
2025-01-18 12:17:41 +0100

    test(boid): add test for constructor and update

--- test_main.cpp
+++ test_main.cpp
@@ -1,7 +1,23 @@
 #include <gtest/gtest.h>
+#include "boid.h"
 
-TEST(SampleTest, Addition) {
-  EXPECT_EQ(2 + 2, 4);
+TEST(Boid, Constructor) {
+  Boid b(100, 200);
+  EXPECT_EQ(b.max_x, 100);
+  EXPECT_EQ(b.max_y, 200);
+}
+
+TEST(Boid, Update) {
+  Boid b(100, 200);
+  float x{b.x};
+  float y{b.y};
+  float vx{b.vx};
+  float vy{b.vy};
+
+  b.update();
+
+  EXPECT_EQ(b.x, x + vx);
+  EXPECT_EQ(b.y, y + vy);
 }
 
 int main(int argc, char** argv) {
