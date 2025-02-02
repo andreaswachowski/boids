e53a99e98524c48f9201b68915f13a5b4b4d44a7
Andreas Wachowski <mail@andreas-wachowski.de>
2025-02-02 22:55:54 +0100

    style: typo in comment

--- src/main.cpp
+++ src/main.cpp
@@ -58,7 +58,7 @@ int main(int argc, char* argv[]) {
     return 1;
   }
 
-  // -- ncursses setup ----------------------------------------------------
+  // -- ncurses setup -----------------------------------------------------
   initscr();
   noecho();
   curs_set(FALSE);
