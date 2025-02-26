eabd0ee8e4c71a96df58ce43146b2f4fa3b1306d
Andreas Wachowski <mail@andreas-wachowski.de>
2025-02-26 21:57:28 +0100

    try compiler options

--- cmake/Config.cmake
+++ cmake/Config.cmake
@@ -11,6 +11,10 @@ include(CheckIPOSupported)
 check_ipo_supported(RESULT ipo_supported)
 set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ${ipo_supported})
 
+if(CMAKE_BUILD_TYPE STREQUAL "Debug" AND CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+  add_compile_options(-fprofile-arcs -ftest-coverage)
+endif()
+
 # Parallel algorithms support for Clang 17+ and operating systems with libc++
 # Primary source:
 # https://libcxx.llvm.org/UserDocumentation.html#enabling-experimental-c-library-features
