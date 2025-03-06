12d6f5a6f2a131b33b18ee9a571b2bf6878026be
Andreas Wachowski <mail@andreas-wachowski.de>
2025-03-06 21:06:04 +0100

    refactor(run): extract config parameters to struct, extract cli parse setup

--- src/cli/run.cpp
+++ src/cli/run.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <memory>
 #include <random>
+#include <string>
 #include <thread>
 #include <vector>
 
@@ -21,36 +22,47 @@
 #include "spdlog/sinks/basic_file_sink.h"
 #include "spdlog/spdlog.h"
 
-namespace {
-void update_boids(std::vector<Boid>& boids) {
-  std::for_each(std::execution::par, boids.begin(), boids.end(), [](Boid& boid) { boid.update(); });
-}
-}  // namespace
-
 constexpr unsigned int DEFAULT_NUM_BOIDS = 20;
 constexpr unsigned int DEFAULT_DELAY_MS = 100;
 
-// NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays, hicpp-avoid-c-arrays, modernize-avoid-c-arrays)
-int run(int argc, char* argv[]) {
-  // -- Command line arguments --------------------------------------------
-  CLI::App app{"Boids"};
-  argv = app.ensure_utf8(argv);
-
+struct SimulationConfig {
   unsigned int seed = INT_MAX;
   unsigned int num_boids = DEFAULT_NUM_BOIDS;
   std::string logfile = "boids.log";
   unsigned int delay_ms = DEFAULT_DELAY_MS;
-  app.add_option("-s,--seed", seed, "Random seed (default random)");
-  app.add_option("-b,--boids", num_boids, "Number of boids (default 20)");
-  app.add_option("-d,--delay", delay_ms, "Delay between iterations (in ms, default 100)");
-  app.add_option("-o,--logfile", logfile,
-                 "Logfile (default \"boids.log\". The file is placed in the "
-                 "directory \"logs\". It is not possible to log to stdout.)");
+};
+
+namespace {
+void update_boids(std::vector<Boid>& boids) {
+  std::for_each(std::execution::par, boids.begin(), boids.end(), [](Boid& boid) { boid.update(); });
+}
+
+SimulationConfig configure_cli_args(CLI::App& app) {
+  SimulationConfig config;
+  app.add_option("-s,--seed", config.seed, "Random seed (default random)");
+  app.add_option("-b,--boids", config.num_boids, "Number of boids (default 20)");
+  app.add_option("-d,--delay", config.delay_ms, "Delay between iterations (in ms, default 100)");
+  app.add_option("-o,--logfile", config.logfile,
+                 R"(Logfile (default "boids.log"). The file is placed in the "logs" directory.)");
   app.set_config("-c,--config", "boids.toml", "Read a config file", false);
 
-  CLI11_PARSE(app, argc, argv);
+  if (config.seed == INT_MAX) {
+    std::random_device rand_device;
+    config.seed = rand_device();
+  }
 
-  std::cout << app.config_to_str(true, true);
+  return config;
+}
+}  // namespace
+
+// NOLINTNEXTLINE(cppcoreguidelines-avoid-c-arrays, hicpp-avoid-c-arrays, modernize-avoid-c-arrays)
+int run(int argc, char* argv[]) {
+  CLI::App app{"Boids"};
+
+  auto config = configure_cli_args(app);
+  argv = app.ensure_utf8(argv);
+
+  CLI11_PARSE(app, argc, argv);
 
   // -- Logger initialization ---------------------------------------------
   spdlog::cfg::load_env_levels();
@@ -58,7 +70,7 @@ int run(int argc, char* argv[]) {
   std::shared_ptr<spdlog::logger> logger;
 
   try {
-    logger = spdlog::basic_logger_mt("basic_logger", "logs/" + logfile);
+    logger = spdlog::basic_logger_mt("basic_logger", "logs/" + config.logfile);
     spdlog::set_default_logger(logger);
   } catch (const spdlog::spdlog_ex& ex) {
     std::cerr << "Log init failed: " << ex.what() << '\n';
@@ -75,18 +87,14 @@ int run(int argc, char* argv[]) {
   int max_y = 0;
   getmaxyx(stdscr, max_y, max_x);
 
-  if (seed == INT_MAX) {
-    std::random_device rand_device;
-    seed = rand_device();
-  }
-  spdlog::info("Using random seed {}.", seed);
+  spdlog::info("Using random seed {}.", config.seed);
   logger->flush();  // ensure that the seed is logged.
 
-  std::mt19937 gen(seed);
+  std::mt19937 gen(config.seed);
 
   std::vector<Boid> boids;
-  boids.reserve(num_boids);
-  for (int i = 0; i < num_boids; ++i) {
+  boids.reserve(config.num_boids);
+  for (int i = 0; i < config.num_boids; ++i) {
     boids.emplace_back(gen, max_x, max_y);
   }
 
@@ -100,7 +108,7 @@ int run(int argc, char* argv[]) {
 
     refresh();
     update_boids(boids);
-    std::this_thread::sleep_for(std::chrono::milliseconds(delay_ms));
+    std::this_thread::sleep_for(std::chrono::milliseconds(config.delay_ms));
   }
 
   endwin();
