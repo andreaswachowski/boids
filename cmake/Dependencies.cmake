aaaca9d4f0b215e34eaca32dc9c83ae5994e8ebe
Andreas Wachowski <mail@andreas-wachowski.de>
2025-03-14 21:52:57 +0100

    build(cmake): add Microsoft.GSL
    I am unsure about hadding Microsoft.GSL::GSL to multiple
    target_link_libraries; and if this is the right approach, then I'd
    likely have to add it later to src/common and test/common as well.
    But it's good enough for now.

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -2,3 +2,7 @@ include(FetchContent)
 FetchContent_Declare(spdlog GIT_REPOSITORY https://github.com/gabime/spdlog.git
                      GIT_TAG v1.15.1 FIND_PACKAGE_ARGS)
 FetchContent_MakeAvailable(spdlog)
+
+FetchContent_Declare(GSL GIT_REPOSITORY https://github.com/microsoft/GSL
+                     GIT_TAG "v4.2.0" GIT_SHALLOW ON)
+FetchContent_MakeAvailable(GSL)
